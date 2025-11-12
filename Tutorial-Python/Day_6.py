c4cf1b74225c9b4e739bf20dc32c696c7d5a78a6
karmanya <karmanya100@gmail.com>
2025-11-12 14:25:53 -0500

    Updated Files , day 1

---
+++ Tutorial-Python/Day_6.py
@@ -0,0 +1,56 @@
+# Variables
+
+# Datatypes stored in RAM
+
+a = 148394349394
+print(a)
+
+b = "\"Karmanya\""
+print(b)
+
+c = True
+
+print(c)
+
+d = None
+print(d)
+
+f1 = 34
+f2 = 78
+
+print(f1+f2)
+
+print("The type of a is : ", type(a))
+print("The type of b is : ", type(b))
+print("The type of c is : ", type(c))
+print("The type of d is : ", type(d))
+
+
+a1 = 1.1
+print("The type of a1 is : ",type(a1))
+
+list1 = [8,2,3,3.4,[-4,5],["apple","banana"]] # Can be changed/ mutable
+print(list1)
+
+tuple1 = ("apple","banana",("dog","cat")) # Cannot be changed/ immutable
+
+print(tuple1)
+
+
+dict1 = {"fruit":"banana","animal":"dog"} # Key value pairs
+
+print(dict1)
+
+# print(list1)
+
+
+# int,float,complex
+# string
+# boolean
+# list,tuple {Sequenced data}
+
+
+
+# every thing in python is an object
+print(type(dict1))
+
