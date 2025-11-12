3cc0a69334e2cfb326d5d2f15ee1f43e3c803bf6
karmanya <karmanya100@gmail.com>
2025-11-12 10:18:58 -0500

    Updated Files , day 1

---
+++ Tutorial-Python/Day_4.py
@@ -0,0 +1,11 @@
+# Our First Python Program
+print("Hello World",100)
+
+print(5)
+
+print("Bye!")
+
+print(17*24)
+
+
+
---
+++ Tutorial-Python/Day_5.py
@@ -0,0 +1,37 @@
+# Comments , Escape Sequence, Print Statement
+
+
+print("I am a good boy\n"
+      "and this viewer is also a good boy")
+
+#\n - New Line
+
+print("This is a print statement")
+
+print("This is ran by python interpreter Pycharm") #Commented Word
+
+
+'''
+
+this is a commented
+paragraph
+type whatever you want to type in your code
+
+
+'''
+
+"""
+Comment the 
+important information
+
+"""
+
+
+print("Hey i am a \"python professional\"  \n and this "
+      "\tperson is also a python professional")
+
+
+print("Hey", 6,7,sep="~", end=" 007\n")
+print("Karmanya")
+
+
