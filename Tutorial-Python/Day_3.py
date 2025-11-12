2e4cebbc26caaf1330b810c440e580b199811623
karmanya <karmanya100@gmail.com>
2025-11-12 09:53:15 -0500

    Updated Files , day 1

---
+++ Tutorial-Python/Day_3.py
@@ -0,0 +1,29 @@
+# Modules & pip
+
+# python3 --version
+#
+# brew install python3
+#
+#
+# pip3 --version
+#
+#
+#
+# Manual Installation
+#
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3 get-pip.py
+
+#pip3 install pandas
+import pandas
+
+
+# pip3 install scikit-learn
+# pip3.13 install --upgrade pip
+import sklearn
+
+
+import hashlib
+# import tensorflow
+
+print("Hello")
